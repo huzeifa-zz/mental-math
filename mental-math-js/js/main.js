00253dacfc046ce75f34af44a512468d0298f538
huzeifa <huzeifa@localhost.localdomain>
2014-03-13 17:01:04 +0300

    Added game logic and UI

--- mental-math-js/js/main.js
+++ mental-math-js/js/main.js
@@ -1,4 +1,5 @@
-$(function(){
+/*$(function(){
+
 	// generate equation and answer
 	var eq = generateEq();
 	var answer = getAnswer(eq);
@@ -46,13 +47,91 @@ function getAnswer(eq){
 	} else {
 		return eq.a - eq.b;
 	}
+}*/
+
+
+var MentalMath = MentalMath || {};
+
+var MentalMath = {
+
+	op_char: ['+', '-'],
+	a: 0, b: 0, op: 0, ans: 0, score: 0,
+	
+	initialize: function() {
+		this.generate();
+		if(this.op === 0) {
+			this.ans = this.add();
+		}
+		else {
+			var tmp = this.a;
+			if(this.a < this.b) {
+				this.a = this.b;
+				this.b = tmp;
+			} 
+			this.ans = this.subtract();
+		}
+	},
+	
+	generate: function() {
+		this.a = this.random(0, 100);
+		this.b = this.random(0, 100);
+		this.op = this.random(0,1);
+	},
+	
+	add: function(){
+		return this.a + this.b;
+	},
+	
+	subtract: function(){		
+		return this.a - this.b
+	},
+	
+	random: function(min, max) { //random function is obtained from underscore.js. All credit goes to underscore.js.
+		if (max == null) {
+			max = min;
+			min = 0;
+		}
+		return min + Math.floor(Math.random() * (max - min + 1));
+	},
+};
+
+
+var start = function() {
+	var d0 = new Date();
+	$('#eq').show();
+	$('#ans').show();
+	$('#btn-start').hide();
+	var interval = setInterval(function(){
+		var d = new Date();
+		var t0 = d.getTime();
+		var countDown = 60 - Math.floor((t0 - d0.getTime()) / 1000);
+		$('#timer').text(countDown);
+		if(countDown === 0) {
+			$('#eq').text('Game Over! Your score: ' + game.score);
+			$('#ans').hide();
+			clearInterval(interval);
+		}
+	}, 1000);
 }
 
-
-var random = function(min, max) {
-    if (max == null) {
-      max = min;
-      min = 0;
-    }
-    return min + Math.floor(Math.random() * (max - min + 1));
- };
+var game = MentalMath;
+$('#eq').hide();
+$('#ans').hide();
+game.initialize();
+$('#eq').text(game.a + ' ' + game.op_char[game.op] + ' ' + game.b);
+$(document).on('keypress', function(e){
+		if(e.which === 13){
+			var user_ans = parseInt($('#ans').val());
+			
+			if(user_ans === game.ans) {
+				game.score++;
+				$('#result').text('correct!');
+				$('#ans').val('');
+				game.initialize();
+				$('#eq').text(game.a + ' ' + game.op_char[game.op] + ' ' + game.b);
+			} else {
+				$('#result').text('incorrect!');
+				$('#ans').val('');
+			}
+		}
+});
